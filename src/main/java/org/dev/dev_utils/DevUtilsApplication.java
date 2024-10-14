34362016fcfdb84bdbcca1a3e772eb4fd47bb790
rwmoliveira <rwmoliveira@gmail.com>
2024-10-14 15:32:06 -0300

    Add files via upload

---
+++ src/main/java/org/dev/dev_utils/DevUtilsApplication.java
@@ -0,0 +1,13 @@
+package org.dev.dev_utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevUtilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dev/dev_utils/DevUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev.dev_utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
